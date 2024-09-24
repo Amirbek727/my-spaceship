c498714e35db7b705695fc812c91523e40d0b32a
Amirbek727 <amirika738@gmail.com>
2024-09-24 13:39:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,177 @@
+// function sayHello(ism = "") {
+//     console.log("Hello" + ism);
+// }
+
+
+// const sayGoodbye = function (ism = "") {
+//     console.log("Goodbye " + ism);
+// }
+
+
+// const sayGoodNight = (nom = "") => console.log("Goodnight " + nom);
+
+// sayGoodNight("Ibrohim");
+// sayGoodNight("Amirbek");
+
+
+// function sayHello(ism) {
+//     console.log("Hello " + ism);
+// }
+
+// function increaseSalary(son) {
+//     console.log(son * 1);
+// }
+
+// const salaries = [1000, 1200, 800, 15000, 1550, 1450, 2000];
+
+// for (let i = 0; i < salaries.length; i++) {
+//     increaseSalary(salaries[i]);
+// }
+
+// salaries.forEach(increaseSalary);
+
+// const myStudents = ["Daler", "Rasul", "Amir", "Alibek", "Mirsaid", "Asilbek", "Ibrohim",];
+// myStudents.forEach(sayHello);
+// myStudents.map(sayHello);
+
+
+// function divide(n) {
+//     console.log(Math.round(n / 2));
+// }
+
+// divide(66) 
+// divide(90)
+
+// const numbers = [42, 98, 55, 11, 5632, 492, 569, 908]
+
+// numbers.forEach(divide);
+
+
+// salaries.(increaseSalary);
+
+
+// const myStudents = ["Daler", "Rasul", "Amir", "Alibek", "Mirsaid", "Asilbek", "Ibrohim",];
+
+// myStudents.forEach(katta);
+
+// function katta() {
+//     console.log(text.toUppercase());
+// }
+
+
+// const myStudents = ["Daler", "Rasul", "Amir", "Alibek", "Mirsaid", "Asilbek", "Ibrohim",];
+
+// const kattaIsmlar = [];
+
+// myStudents.forEach((text) => {
+//     kattaIsmlar.push(text.toUpperCase());
+// });
+
+// console.log(kattaIsmlar);
+
+
+
+
+// const numbers = [0, 2, 4, 6, 8, 10];
+
+// const kattaNumbers = [];
+
+// numbers.forEach((son)=>{
+//     kattaNumbers.push(son * 10);
+// });
+
+// console.log(kattaNumbers);
+
+
+// const numbers2 = [0, 2, 4, 6, 8, 10];
+
+// const kattaNumbers2 = [];
+
+// numbers2.forEach((son)=>{
+//     kattaNumbers2.push(son - 10);
+// });
+
+// console.log(kattaNumbers2);
+
+const spaceship = {
+    "name": "7Ways",
+    "fuel type": "gas",
+    color: "Uzbekistan's flag",
+    team: 7,
+    creator: "Amirbek",
+    fly: "Mars",
+    "homeplanet": "Earth",
+     
+}
+
+let x = "team"
+
+console.log(spaceship);
+function Logo() {
+    console.log(`
+
+        
+     █████████████       ███          ███           
+     ███       ███       ███          ███           
+     ███       ███       ███          ███           
+               ███       ███    ███   ███           
+            █████████    ███    ███   ███           
+               ███       ███    ███   ███           
+               ███       ███    ███   ███       
+               ███       ███    ███   ███       
+               ███       ████████████████       
+            
+
+
+         ███████████     ███     ███    
+         ███     ███     ███     ███
+         ███     ███     ███     ███
+         ███     ███     ███     ███
+         ███████████     ███     ███
+         ███     ███     ███████████  
+         ███     ███         ███
+         ███     ███         ███
+         ███     ███         ███
+
+
+
+    `)
+}
+
+ Logo();
+ 
+
+
+
+
+
+    //         ███████╗██╗   ██╗ ██████╗ ██████╗
+    //         ██╔════╝██║   ██║██╔════╝ ██╔════╝
+    //         █████╗  █║   ██║██║  ███╗█████╗  
+    //         ██╔══╝  ██║   ██║██║   ██║██╔══╝  
+    //         ███████╗╚██████╔╝╚██████╔╝███████╗
+    //         ╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝
+
+
+
+    // █████████████
+    // ███       ███  
+    // ███       ███
+    //           ███        
+    //           ███
+    //           ███
+    //           ███
+    //           ███
+    //           ███
+
+    // █████████████       ███          ███      ███████████      ███     ███
+    // ███       ███       ███          ███      ███     ███      ███     ███
+    // ███       ███       ███          ███      ███     ███      ███     ███
+    //           ███       ███    ███   ███      ███     ███      ███     ███
+    //        █████████    ███    ███   ███      ███████████      ███     ███
+    //           ███       ███    ███   ███      ███     ███      ███████████
+    //           ███       ███    ███   ███      ███     ███          ███
+    //           ███       ███    ███   ███      ███     ███          ███
+    //           ███       ████████████████      ███     ███          ███
+           
+
